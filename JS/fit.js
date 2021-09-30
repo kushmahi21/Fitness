b293163253f9155eeae310fec2e8389c569d3f2f
kushmahi21 <64887894+kushmahi21@users.noreply.github.com>
2021-09-30 15:17:28 +0530

    Add files via upload

---
+++ JS/fit.js
@@ -0,0 +1,9 @@
+  
+function toggleMenu(){
+    var menuToggle = document.querySelector(".toggle");
+     menuToggle.classList.toggle('active');
+     var banner = document.querySelector(".banner");
+     banner.classList.toggle('active');
+   
+
+}
