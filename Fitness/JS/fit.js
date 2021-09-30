d841c464e458f7410591cec6e3eb6ee99cb2fec4
kushmahi21 <64887894+kushmahi21@users.noreply.github.com>
2021-09-30 15:10:58 +0530

    Delete Fitness directory

--- Fitness/JS/fit.js
+++
@@ -1,9 +0,0 @@
-  
-function toggleMenu(){
-    var menuToggle = document.querySelector(".toggle");
-     menuToggle.classList.toggle('active');
-     var banner = document.querySelector(".banner");
-     banner.classList.toggle('active');
-   
-
-}
